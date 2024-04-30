dc1e4651b6d7cfa9fabb6ba6502ad717293b3e17
Hojoon LEE <joonlee031@live.com>
2024-04-30 14:24:29 +0200

    Up

---
+++ srcs/requirements/mariadb/mariadb_script.sh
@@ -0,0 +1,11 @@
+echo "CREATE USER '$MYSQL_USER'@%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
+echo "FLUSH PRIVILEGES;" | mysql
+
+echo "CREATE DATABASE WORDPRESS;" | mysql
+
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
