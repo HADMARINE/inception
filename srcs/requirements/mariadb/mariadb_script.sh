bc4b7f2293ac12a5719936780eabf2d84298317f
Hojoon LEE <joonlee031@live.com>
2024-04-30 15:03:39 +0200

    d

--- srcs/requirements/mariadb/mariadb_script.sh
+++ srcs/requirements/mariadb/mariadb_script.sh
@@ -1,7 +1,5 @@
 service mariadb start
 
-echo $MYSQL_USER; exit 1
-
 echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
 
 echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
