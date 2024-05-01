03d61e95a32a6039e6b164c606b4359c99373a05
Hojoon LEE <joonlee031@live.com>
2024-05-01 15:30:05 +0200

    d

--- srcs/requirements/mariadb/mariadb_script.sh
+++ srcs/requirements/mariadb/mariadb_script.sh
@@ -1,24 +1,23 @@
-export DEBIAN_FRONTEND="noninteractive"
+# export DEBIAN_FRONTEND="noninteractive"
 
 # mysql -uroot -pPASS -e "SET PASSWORD = PASSWORD('$MYSQL_ROOT_PASSWORD');"
-debconf-set-selections <<< "mariadb-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
-debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD" 
+# debconf-set-selections <<< "mariadb-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
+# debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD" 
 
-apt -y install mariadb-server
 
 # mysql_secure_installation
 
-service mariadb start
+service mysql start
 
-echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mariadb
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
 
-echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mariadb
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mariadb
-echo "FLUSH PRIVILEGES;" | mariadb
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
+echo "FLUSH PRIVILEGES;" | mysql
 
-echo "CREATE DATABASE WORDPRESS;" | mariadb
+echo "CREATE DATABASE WORDPRESS;" | mysql
 
-# kill $(cat /var/run/mysqld/mysqld.pid)
-service mariadb start
+kill $(cat /var/run/mysqld/mysqld.pid)
+# service mysql stop
 
-mariadbd
+mysqld
