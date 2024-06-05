2cf6b57c5b1fed9457d257760ef119ffdf0a3fbf
Hojoon LEE <joonlee031@live.com>
2024-06-05 17:34:45 +0200

    dd

--- srcs/requirements/wordpress/install.sh
+++ srcs/requirements/wordpress/install.sh
@@ -12,10 +12,14 @@ else
 	rm -rf latest.tar.gz
 	rm -rf wordpress
 
-	sed -i "s/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/$MYSQL_DATABASE/g" wp-config-sample.php
+	# sed -i "s/$MYSQL_USER/g" wp-config-sample.php
+	# sed -i "s/$MYSQL_PASSWORD/g" wp-config-sample.php
+	# sed -i "s/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	# sed -i "s/$MYSQL_DATABASE/g" wp-config-sample.php
+    sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
 
 fi
