87a6d00ddb94d4ebcf142f7516e5f1ddd0336466
Hojoon LEE <joonlee031@live.com>
2024-06-06 17:16:31 +0200

    d

--- srcs/requirements/wordpress/install.sh
+++ srcs/requirements/wordpress/install.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-#check if wp-config.php exist
-if [ -f ./wp-config.php ]
-then
-	echo "wordpress already downloaded"
-else
 
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
+	# wget http://wordpress.org/latest.tar.gz
+	# tar xfz latest.tar.gz
+	# mv wordpress/* .
+	# rm -rf latest.tar.gz
+	# rm -rf wordpress
 
 	# sed -i "s/$MYSQL_USER/g" wp-config-sample.php
 	# sed -i "s/$MYSQL_PASSWORD/g" wp-config-sample.php
 	# sed -i "s/$MYSQL_HOSTNAME/g" wp-config-sample.php
 	# sed -i "s/$MYSQL_DATABASE/g" wp-config-sample.php
-    sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
+sed -i -r "s/db1/$MYSQL_DATABASE/1"   /var/www/html/lhojoon.42.fr/wp-config.php
+sed -i -r "s/user/$MYSQL_USER/1"  /var/www/html/lhojoon.42.fr/wp-config.php
+sed -i -r "s/pwd/$MYSQL_PASSWORD/1"    /var/www/html/lhojoon.42.fr/wp-config.php
 
-fi
+wp core install --url=$WP_HOSTNAME/ --title=$WP_TITLE -admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+wp theme install astra --activate --allow-root
+wp plugin update --all --allow-root
 
-exec "$@"
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
+
+mkdir -p /run/php
+
+	# cp wp-config-sample.php wp-config.php
