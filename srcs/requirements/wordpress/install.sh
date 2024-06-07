df3a42804eea78a0eb7a6466eeefe8ad01fc8b7a
Hojoon LEE <joonlee031@live.com>
2024-06-07 17:24:07 +0200

    d

--- srcs/requirements/wordpress/install.sh
+++ srcs/requirements/wordpress/install.sh
@@ -11,14 +11,21 @@
 	# sed -i "s/$MYSQL_PASSWORD/g" wp-config-sample.php
 	# sed -i "s/$MYSQL_HOSTNAME/g" wp-config-sample.php
 	# sed -i "s/$MYSQL_DATABASE/g" wp-config-sample.php
+
+rm -rf /etc/php/7.4/fpm/pool.d/www.conf
+mv ./www.conf /etc/php/7.4/fpm/pool.d/
+
 sed -i -r "s/db1/$MYSQL_DATABASE/1"   lhojoon.42.fr/wp-config.php
 sed -i -r "s/user/$MYSQL_USER/1"  lhojoon.42.fr/wp-config.php
 sed -i -r "s/pwd/$MYSQL_PASSWORD/1"    lhojoon.42.fr/wp-config.php
+mv wp-config.php ./lhojoon.42.fr/wp-config.php
 
-wp core install --url=$WP_HOSTNAME/ --title=$WP_TITLE -admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-wp theme install astra --activate --allow-root
-wp plugin update --all --allow-root
+cd lhojoon.42.fr
+
+# wp core install --url=$WP_HOSTNAME/ --title=$WP_TITLE -admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+# wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+# wp theme install astra --activate --allow-root
+# wp plugin update --all --allow-root
 
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
