9392a46e17209530bf70cab730ac945a043ef47e
Hojoon LEE <joonlee031@live.com>
2024-06-07 18:00:43 +0200

    d

--- srcs/requirements/wordpress/install.sh
+++ srcs/requirements/wordpress/install.sh
@@ -1,33 +1,25 @@
 #!/bin/sh
 
+if [ -f ./wordpress/wp-config.php ]
+then
+	echo "wordpress already downloaded"
+else
+	#Download wordpress
+	wget https://wordpress.org/latest.tar.gz
+	tar -xzvf latest.tar.gz
+	rm -rf latest.tar.gz
 
-	# wget http://wordpress.org/latest.tar.gz
-	# tar xfz latest.tar.gz
-	# mv wordpress/* .
-	# rm -rf latest.tar.gz
-	# rm -rf wordpress
+	#Update configuration file
+	rm -rf /etc/php/7.3/fpm/pool.d/www.conf
+	mv ./www.conf /etc/php/7.3/fpm/pool.d/
 
-	# sed -i "s/$MYSQL_USER/g" wp-config-sample.php
-	# sed -i "s/$MYSQL_PASSWORD/g" wp-config-sample.php
-	# sed -i "s/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	# sed -i "s/$MYSQL_DATABASE/g" wp-config-sample.php
+	#Inport env variables in the config file
+	cd /var/www/html/wordpress
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	mv wp-config-sample.php wp-config.php
+fi
 
-# mv ./www.conf /etc/php/7.4/fpm/pool.d/
-
-sed -i -r "s/db1/$MYSQL_DATABASE/1"   lhojoon.42.fr/wp-config.php
-sed -i -r "s/user/$MYSQL_USER/1"  lhojoon.42.fr/wp-config.php
-sed -i -r "s/pwd/$MYSQL_PASSWORD/1"    lhojoon.42.fr/wp-config.php
-mv wp-config.php ./lhojoon.42.fr/wp-config.php
-
-cd lhojoon.42.fr
-
-# wp core install --url=$WP_HOSTNAME/ --title=$WP_TITLE -admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-# wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-# wp theme install astra --activate --allow-root
-# wp plugin update --all --allow-root
-
-sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
-
-mkdir -p /run/php
-
-	# cp wp-config-sample.php wp-config.php
+exec "$@"
