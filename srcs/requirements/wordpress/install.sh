5698de067380b3a004181e46e15b12f496e52461
Hojoon LEE <joonlee031@live.com>
2024-06-12 15:36:25 +0200

    d

--- srcs/requirements/wordpress/install.sh
+++ srcs/requirements/wordpress/install.sh
@@ -9,6 +9,15 @@ else
 	tar -xzvf latest.tar.gz
 	rm -rf latest.tar.gz
 
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+
+	chmod +x wp-cli.phar 
+
+	mv wp-cli.phar /usr/local/bin/wp
+
+
+	wp core download --allow-root
+
 	#Update configuration file
 	rm -rf /etc/php/7.3/fpm/pool.d/www.conf
 	mv ./www.conf /etc/php/7.3/fpm/pool.d/
@@ -21,6 +30,7 @@ else
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 	mv wp-config-sample.php wp-config.php
 	wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+	# wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 fi
 
 exec "$@"
