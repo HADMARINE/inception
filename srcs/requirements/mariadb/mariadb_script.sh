f8bf407df79e1cc71b082441b2ac5c5ceb3aaacd
Hojoon LEE <joonlee031@live.com>
2024-06-12 14:53:43 +0200

    d

--- srcs/requirements/mariadb/mariadb_script.sh
+++ srcs/requirements/mariadb/mariadb_script.sh
@@ -11,13 +11,21 @@
 
 service mariadb start
 
-echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+# echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
 
-echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
-echo "FLUSH PRIVILEGES;" | mysql
+# echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+# echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
+# echo "FLUSH PRIVILEGES;" | mysql
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql
+# echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql
+
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+
+mysql < db1.sql
 
 kill $(cat /var/run/mysqld/mysqld.pid)
 # service mysql stop
